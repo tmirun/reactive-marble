3b5c1af1869a004ffba4f4d7ddf6f29dec79f7a5
tmirun <tmirun@hotmail.com>
2018-02-18 17:41:06 +0100

    regenerate app with router and sass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'rm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'rm';
 }
