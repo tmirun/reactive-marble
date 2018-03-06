9eb813094d75a7d90f618b671471648618e2ce81
tmirun <tmirun@hotmail.com>
2018-03-06 18:19:23 +0100

    add marble component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MarbleComponent } from './marble/marble.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  exports: [
+    MarbleComponent
+  ],
+  declarations: [MarbleComponent]
 })
 export class CoreModule { }
