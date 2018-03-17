ccc7298c23ffd89478cace78e0bfe3f272843d38
tmirun <tmirun@hotmail.com>
2018-03-17 20:33:13 +0100

    redirect to creator/create url by default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 const routes: Routes = [
+  { path: '', redirectTo: '/creators/create', pathMatch: 'full' },
   {
     path: ':category/:operator',
     component: MainComponent,
